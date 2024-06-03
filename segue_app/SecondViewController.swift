34a112dd67c7ac3558bcd384d233fe1fc8714c74
Rodrigo Silva <18017250+Crewlan@users.noreply.github.com>
2024-06-02 21:21:59 -0300

    Create project and create navigation to undestading how to navigate using swift button

--- segue_app/SecondViewController.swift
+++ segue_app/SecondViewController.swift
@@ -9,10 +9,16 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var secondLabel: UILabel!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var myName = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        nameLabel.text = "name: \(myName)"
     }
     
 
